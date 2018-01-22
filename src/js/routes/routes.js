2e0ca4188623e3531c2a7eb23565b58ffb56a679
zuri.hunter <zuri.hunter@digitalglobe.com>
2018-01-22 00:58:18 -0500

    Error with webpack-server connection for the project

--- src/js/routes/routes.js
+++ src/js/routes/routes.js
@@ -1,1 +1,1 @@
-module.exports = {}
+console.log("I am in!");
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const resolve = require('path').resolve;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './js/index.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/speakincolor/',
+  },
+  context: resolve(__dirname, 'src'),
+  devtool: 'eval',
+  bail: false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+               presets: ['es2015', 'react']
+           }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              localIdentName: '[name].[ext]',
+            },
+          },
+        }),
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+        }],
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        loader: 'file-loader?hash=sha512&digest=hex&name=[name].[ext]',
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=[name].[ext]',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.ejs',
+      baseUrl: '/speakincolor/',
+      inject: false,
+    }),
+    new CopyWebpackPlugin([{
+      from: './*.*',
+      to: resolve(__dirname, 'dist'),
+      ignore: ['*.ejs'],
+    }]),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.NamedModulesPlugin(),
+  ],
+  devServer: {
+    historyApiFallback: {
+      index: '/speakincolor/index.html',
+    },
+    port: 8000,
+    https: true,
+    public: 'localhost',
+    publicPath: '/speakincolor/',
+  },
+};
